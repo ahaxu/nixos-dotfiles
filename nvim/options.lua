7bcf7ec3ae2b5c1fe94e3779717e387ed03192bf
ahaxu <banghahatinh96@gmail.com>
2024-02-14 13:55:18 +0700

    Start using telescope

--- nvim/options.lua
+++ nvim/options.lua
@@ -1,18 +1,74 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+local g = vim.g
+g.mapleader = " "
 
-vim.o.clipboard = 'unnamedplus'
+-- misc utils
+local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
 
-vim.o.number = true
--- vim.o.relativenumber = true
+local function opt(scope, key, value)
+    scopes[scope][key] = value
+    if scope ~= "o" then
+        scopes["o"][key] = value
+    end
+end
 
-vim.o.signcolumn = 'yes'
+local function map(mode, lhs, rhs, opts)
+    local options = {noremap = true}
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
 
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
+opt("o", "hidden", true)
+opt("o", "ignorecase", true)
+opt("o", "splitbelow", true)
+opt("o", "splitright", true)
+opt("o", "termguicolors", true)
+opt("w", "number", true)
+--opt("w", "relativenumber", true)
+opt("o", "numberwidth", 2)
+opt("b", "undofile", true)
+opt("b", "textwidth", 88)
+opt("w", "wrap", true)
+opt("w", "cursorline", true)
+opt("w", "colorcolumn", "+1")
 
-vim.o.updatetime = 300
+opt("o", "mouse", "a")
+opt("b", "spelllang", "en,fr")
 
-vim.o.termguicolors = true
 
-vim.o.mouse = 'a'
+opt("w", "signcolumn", "yes")
+opt("o", "cmdheight", 1)
+
+opt("o", "updatetime", 250) -- update interval for gitsigns
+opt("o", "clipboard", "unnamedplus")
+
+-- for indenline
+opt("b", "expandtab", true)
+opt("b", "shiftwidth", 4)
+
+-- folding
+opt("o", "foldlevelstart", 50)  -- open most folds by default
+opt("w", "foldnestmax", 2)  -- 2 nested fold max
+
+
+
+
+-- tab configuration
+-- buffer configuration
+local opt = {silent = true}
+
+local map = vim.api.nvim_set_keymap
+vim.g.mapleader = " "
+
+--command that adds new buffer and moves to it
+map("n", "<C-t>", [[<Cmd>tabnew<CR>]], opt)
+
+--removing a buffer
+map("n", "<S-x>", [[<Cmd>bdelete<CR>]], opt)
+
+-- tabnew and tabprev
+map("n", "<S-t>", [[<Cmd>BufferLineCycleNext<CR>]], opt)
+map("n", "<S-n>", [[<Cmd>BufferLineCyclePrev<CR>]], opt)
+map("n", "gb", [[<Cmd>BufferLinePick<CR>]], opt)
+
--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -10,4 +10,32 @@ require('telescope').setup({
   	}
 })
 
+local cmd = vim.cmd
+local g = vim.g
+
+g.mapleader = " "
+
+local opt = {noremap = true, silent = true}
+
+local function map(mode, lhs, rhs, opts)
+    local options = {noremap = true}
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-- mappings
+map("n", "<Leader>ff", [[<Cmd>lua require('telescope.builtin').find_files()<CR>]], opt)
+map("n", "<Leader>fb", [[<Cmd>lua require('telescope.builtin').buffers()<CR>]], opt)
+map("n", "<Leader>fg", [[<Cmd>lua require('telescope.builtin').live_grep()<CR>]], opt)
+map("n", "<Leader>fs", [[<Cmd>lua require('telescope.builtin').spell_suggest()<CR>]], opt)
+map("n", "<Leader>fa", [[<Cmd>lua require('telescope.builtin').lsp_code_actions()<CR>]], opt)
+map("n", "<Leader>fh", [[<Cmd>lua require('telescope.builtin').help_tags()<CR>]], opt)
+map("n", "<Leader>fo", [[<Cmd>lua require('telescope.builtin').oldfiles()<CR>]], opt)
+map("n", "<Leader>fe", [[<Cmd>lua require('telescope.builtin').symbols()<CR>]], opt)
+map("n", "<Leader>ft", [[<Cmd>lua require('telescope.builtin').treesitter()<CR>]], opt)
+map("n", "<Leader>fm", [[<Cmd> Neoformat<CR>]], opt)
+
+
 require('telescope').load_extension('fzf')
