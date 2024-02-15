761e9fbd8a77390cd53d0597fc614bf15465ed57
ahaxu <banghahatinh96@gmail.com>
2024-02-15 07:50:43 +0700

    Toggle neotree

--- nvim/plugin/neo-tree.lua
+++ nvim/plugin/neo-tree.lua
@@ -19,7 +19,7 @@ require("neo-tree").setup({
     enable_diagnostics = true,
     open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
     sort_case_insensitive = false, -- used when sorting files and directories in the tree
-    sort_function = nil , -- use a custom function for sorting files and directories in the tree 
+    sort_function = nil , -- use a custom function for sorting files and directories in the tree
     -- sort_function = function (a,b)
     --       if a.type == b.type then
     --           return a.path > b.path
@@ -91,9 +91,9 @@ require("neo-tree").setup({
         nowait = true,
       },
       mappings = {
-        ["<space>"] = { 
-            "toggle_node", 
-            nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use 
+        ["<space>"] = {
+            "toggle_node",
+            nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
         },
         ["<2-LeftMouse>"] = "open",
         ["<cr>"] = "open",
@@ -113,7 +113,7 @@ require("neo-tree").setup({
         -- ['C'] = 'close_all_subnodes',
         ["z"] = "close_all_nodes",
         --["Z"] = "expand_all_nodes",
-        ["a"] = { 
+        ["a"] = {
           "add",
           -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
           -- some commands may take optional config options, see `:h neo-tree-mappings` for details
@@ -167,7 +167,9 @@ require("neo-tree").setup({
           --".null-ls_*",
         },
       },
-      follow_current_file = true, -- This will find and focus the file in the active buffer every
+      follow_current_file = {
+            enabled
+      }, -- This will find and focus the file in the active buffer every
                                   -- time the current file is changed while the tree is open.
       group_empty_dirs = false, -- when true, empty folders will be grouped together
       hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
@@ -202,7 +204,9 @@ require("neo-tree").setup({
       commands = {} -- Add a custom command or override a global one using the same function name
     },
     buffers = {
-      follow_current_file = true, -- This will find and focus the file in the active buffer every
+      follow_current_file = {
+        enabled
+      }, -- This will find and focus the file in the active buffer every
                                    -- time the current file is changed while the tree is open.
       group_empty_dirs = true, -- when true, empty folders will be grouped together
       show_unloaded = true,
@@ -243,5 +247,6 @@ local function map(mode, lhs, rhs, opts)
 end
 
 vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
-map("n", "<Leader>ne", [[<Cmd>Neotree toggle<CR>]], opt)
+map("n", "<Leader>ne", [[<Cmd>Neotree<CR>]], opt)
+map("n", "<Leader>nt", [[<Cmd>Neotree toggle<CR>]], opt)
 
